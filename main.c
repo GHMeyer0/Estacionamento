9bc88803a9986e62038843b39fce8ab6799c8d7d
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-11-05 12:50:37 -0300

    Testes de vagas

--- main.c
+++ main.c
@@ -11,40 +11,38 @@ typedef struct
     sem_t disponivel;
 } Vagas;
 
-Vagas vaga[];
-
-
-void CriarVagas(int quantidade_de_vagas){
-    for (int i = 0; i <= quantidade_de_vagas; i++)
-    {
-        vaga[i].numero = i;
-        sem_init(&vaga[i].disponivel, 0, 1);
-    }
-}
-
-void EntrarCarro(){
-
-}
-
-void SairCarro(){
-
-}
+Vagas _vaga[];
 
 int main() {
     CriarVagas(10);
     for (size_t i = 0; i < 10; i++)
     {
-        printf("O Numero é %d \n", vaga[i].numero);
-       sem_wait(&vaga[i].disponivel);
-       sem_post(&vaga[i].disponivel);
-       if( sem_trywait(&vaga[i].disponivel)){
+        printf("O Numero é %d \n", _vaga[i].numero);
+       sem_wait(&_vaga[i].disponivel);
+       sem_post(&_vaga[i].disponivel);
+       if( sem_trywait(&_vaga[i].disponivel)){
            printf("TRUE");
        } else
        {
            printf("FALSE");
        }
     }
-    
+}
+
+
+
+void CriarVagas(int quantidade_de_vagas){
+    for (int i = 0; i <= quantidade_de_vagas; i++)
+    {
+        _vaga[i].numero = i;
+        sem_init(&_vaga[i].disponivel, 0, 1);
+    }
+}
+
+void EntrarCarro(){
+}
+
+void SairCarro(){
 
 }
 
