6ea384f76899e987eaaed4288343242bdbc95c22
Azure DevOps <azuredevops@microsoft.com>
2019-11-27 15:35:21 -0300

    Ok

--- Python/main.py
+++ Python/main.py
@@ -3,28 +3,29 @@ import vagas
 import threading as t
 import time
 import random
+import os
+
+
+def clear(): return os.system('cls')  # on Windows System
 
 estacionamento = vagas.Vaga(10)
-
-#cancelas = {
-#    cancela1: t.Thread(target=thread_function, args=(1,)),
-#        
-#}
-
 class Cancela(t.Thread):
-    def carroEntrar(self):
-        estacionamento.ocupaVaga()
-    
-    def carroSair(self):
-        estacionamento.liberaVaga()
+    def carroEntrar(self, quantidade_de_carros):
+        for target_list in range(quantidade_de_carros):
+            estacionamento.ocupaVaga()
+        
+    def carroSair(self, quantidade_de_carros):
+        for target_list in range(quantidade_de_carros):
+            estacionamento.liberaVaga()
+        
 
     def run(self):
         while True:
             IO = random.randint(0, 1)
             if IO == 1:
-                self.carroEntrar()
+                self.carroEntrar(random.randint(1,5))
             if IO == 0:
-                self.carroSair()
+                self.carroSair(random.randint(1,5))
         return
 
 for i in range(10):
