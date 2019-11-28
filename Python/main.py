eb9b3641346f8bc623f99c19cb3061e3685b86cc
Azure DevOps <azuredevops@microsoft.com>
2019-11-27 21:06:28 -0300

    Commit Monstro
    Co-authored-by: rrschiavofilho@gmail.com <rrschiavofilho@gmail.com>

--- Python/main.py
+++ Python/main.py
@@ -9,33 +9,48 @@ import os
 def clear(): return os.system('cls')  # on Windows System
 
 estacionamento = vagas.Vaga(10)
+fila_pra_entrar = 20
+fila_pra_sair = 0
+
 class Cancela(t.Thread):
     def carroEntrar(self, quantidade_de_carros):
         for target_list in range(quantidade_de_carros):
             estacionamento.ocupaVaga()
+
         
     def carroSair(self, quantidade_de_carros):
         for target_list in range(quantidade_de_carros):
             estacionamento.liberaVaga()
-        
+
 
     def run(self):
-        while True:
-            IO = random.randint(0, 1)
+        global fila_pra_entrar
+        global fila_pra_sair
+        while fila_pra_entrar > 0:
+            if fila_pra_sair > 5:
+                IO = 0
+            else: IO = 1
             if IO == 1:
-                self.carroEntrar(random.randint(1,5))
+                self.carroEntrar(1)
+                fila_pra_entrar -= 1
+                fila_pra_sair += 1
             if IO == 0:
-                self.carroSair(random.randint(1,5))
+                self.carroSair(1)
+                fila_pra_sair -= 1
         return
-
-##for i in range(10):
-##	cancela = Cancela()
-##	cancela.start()
+    
 
 class Application:
     fontPadrao = ("Verdana", "10", "italic")
     fontPadraoNegrito = ("Verdana", "10", "italic", "bold")
     def __init__(self, master=None):
+        def callback():
+            print(self.quantCarrosEntrar.get())
+            cancela = Cancela()
+            for target_list in range(3):
+                cancela = Cancela()
+                cancela.start()
+            
         self.mainContainer = Frame(master)
         self.mainContainer.pack()
         self.msg = Label(self.mainContainer, text="Estacionamento")
@@ -54,10 +69,12 @@ class Application:
         self.sairMsg = Label(self.mainContainer, text="Quantidade de carros")
         self.sairMsg["font"] = self.fontPadrao
         self.sairMsg.pack()
+
         self.quantCarrosSair = Entry(self.mainContainer)
         self.quantCarrosSair["width"] = 10
         self.quantCarrosSair["font"] = self.fontPadrao
         self.quantCarrosSair.pack(side=TOP)
+
         self.sair = Button(self.mainContainer)
         self.sair["text"] = "Sair carro"
         self.sair["font"] = ("Verdana", "10")
@@ -68,15 +85,16 @@ class Application:
         self.entrarMsg = Label(self.mainContainer, text="Quantidade de carros")
         self.entrarMsg["font"] = self.fontPadrao
         self.entrarMsg.pack()
+
         self.quantCarrosEntrar = Entry(self.mainContainer)
         self.quantCarrosEntrar["width"] = 10
         self.quantCarrosEntrar["font"] = self.fontPadrao
         self.quantCarrosEntrar.pack(side=TOP)
-        self.entrar = Button(self.mainContainer)
+        self.entrar = Button(self.mainContainer, command=callback)
         self.entrar["text"] = "Entrar carro"
         self.entrar["font"] = self.fontPadrao
         self.entrar["width"] = 10
-        self.entrar.pack ()
+        self.entrar.pack()
 
 root = Tk()
 Application(root)
