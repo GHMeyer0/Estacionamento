888811769319839db5dd7a27c867f44d01002284
Renato <you@example.com>
2019-11-28 16:33:24 -0300

    feito toda essa merda

--- Python/main.py
+++ Python/main.py
@@ -8,13 +8,28 @@ import os
 
 def clear(): return os.system('cls')  # on Windows System
 
-
+fila_pra_entrar = 0
+fila_pra_sair = 0 
+quantidade_vagas = 0 
 lock = threading.Semaphore(1)
-estacionamento = vagas.Vaga(10)
-fila_pra_entrar = 1000
-fila_pra_sair = 0
+estacionamento = vagas.Vaga(quantidade_vagas)
 
-class Cancela(t.Thread): 
+
+def setQuantCancelas(quantidade):
+    quantidade = int(quantidade)
+    cancela = {}
+    num = 0
+    for index in range(quantidade - 1):
+        num += 1
+        cancela.update({num: Cancela()})
+    iniciaCancela(cancela)
+
+def iniciaCancela(cancelasParaIniciar):
+    for target_list in (cancelasParaIniciar):
+        cancela = cancelasParaIniciar[target_list]
+        cancela.start()
+
+class Cancela(t.Thread):
     global fila_pra_entrar
     global fila_pra_sair 
 
@@ -26,13 +41,12 @@ class Cancela(t.Thread):
         estacionamento.ocupaVaga() 
         fila_pra_entrar -= 1
         fila_pra_sair += 1
-        lock.release()
-    
+        lock.release() 
 
     def carroSair(self):
         global fila_pra_entrar
         global fila_pra_sair 
-
+        lock.acquire()
         estacionamento.liberaVaga()
         fila_pra_sair -= 1
         lock.release()
@@ -43,9 +57,7 @@ class Cancela(t.Thread):
             if InputOutput == 1:
                 self.carroEntrar()
             if InputOutput == 0:
-                self.carroSair()
-            
-        return
+                self.carroSair()  
 
 
 
@@ -53,16 +65,28 @@ class Application:
     fontPadrao = ("Verdana", "10", "italic")
     fontPadraoNegrito = ("Verdana", "10", "italic", "bold")
 
+    def setQuantVagasEntry(self):
+        global quantidade_vagas
+        quantidade_vagas = self.quantVagas.get()
+        quantidade_vagas = int(quantidade_vagas)
+    
+    def setQuantCarrosEntry(self):
+        global fila_pra_entrar
+        fila_pra_entrar = self.quantCarrosEntrar.get()
+        fila_pra_entrar = int(fila_pra_entrar)
+    
+    def setQuantCancelasEntry(self):
+        global estacionamento
+        self.setQuantVagasEntry()
+        estacionamento = vagas.Vaga(quantidade_vagas)
+        self.setQuantCarrosEntry()
+        quantCancelas = self.quantCancelas.get()
+        setQuantCancelas(quantCancelas)
+        return quantCancelas
+
     def __init__(self, master=None):
         global fila_pra_sair
-        fila_pra_sair_str = str(fila_pra_sair)
-
-        def callback():
-            print(self.quantCarrosEntrar.get())
-            cancela = Cancela()
-            for target_list in range(3):
-                cancela = Cancela()
-                cancela.start()
+        global fila_pra_entrar
 
         self.mainContainer = Frame(master)
         self.mainContainer["width"] = 60
@@ -71,16 +95,23 @@ class Application:
         self.msg["font"] = self.fontPadraoNegrito
         self.msg.pack ()
 
+        self.quantVagasMsg = Label(self.mainContainer, text="Quantidade de vagas")
+        self.quantVagasMsg["font"] = self.fontPadrao
+        self.quantVagasMsg.pack()
+        self.quantVagas = Entry(self.mainContainer)
+        self.quantVagas["width"] = 40
+        self.quantVagas["font"] = self.fontPadrao
+        self.quantVagas.pack(side=TOP)
+
         self.quantCancelasMsg = Label(self.mainContainer, text="Quantidade de cancelas")
         self.quantCancelasMsg["font"] = self.fontPadrao
         self.quantCancelasMsg.pack()
-
         self.quantCancelas = Entry(self.mainContainer)
         self.quantCancelas["width"] = 40
         self.quantCancelas["font"] = self.fontPadrao
         self.quantCancelas.pack(side=TOP)
 
-        self.quantCarrosMsg = Label(self.mainContainer, text="Quantidade de carros")
+        self.quantCarrosMsg = Label(self.mainContainer, text="Quantidade de carros para entrar")
         self.quantCarrosMsg["font"] = self.fontPadrao
         self.quantCarrosMsg.pack()
         self.quantCarrosEntrar = Entry(self.mainContainer)
@@ -88,17 +119,12 @@ class Application:
         self.quantCarrosEntrar["font"] = self.fontPadrao
         self.quantCarrosEntrar.pack(side=TOP)
 
-        self.entrar = Button(self.mainContainer, command=callback)
+        self.entrar = Button(self.mainContainer)
+        self.entrar["command"] = self.setQuantCancelasEntry
         self.entrar["text"] = "Entrar carros"
         self.entrar["font"] = self.fontPadrao
         self.entrar["width"] = 20
         self.entrar.pack()
-
-        ##self.quantidadeDeVagasOcupadasMsg = Label(self.mainContainer, text=fila_pra_sair_str)
-        ##self.quantidadeDeVagasOcupadasMsg["font"] = self.fontPadraoNegrito
-        ##self.quantidadeDeVagasOcupadasMsg.pack()
-
-
 
 
 root = Tk()
